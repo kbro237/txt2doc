58c42f9ae2975a9be4fbd72c83d36eeb6b90b22a
Keith Walbolt <keith.walbolt@gmail.com>
2012-09-20 12:46:09 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This script process all txt files in a directory thru mmd and converts the
+output to a Word Document in the same directory.
+
+Example: a folder is full of txt newsletter articles that I've written, but
+now I have to give them to someone else...
---
+++ txt2doc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script process all txt files in a directory thru mmd and converts the
+# output to a Word Document in the same directory example: a folder is full of
+# txt newsletter articles that I've written, but now I have to give them to
+# someone else...
+
+for file in *.txt
+do
+    multimarkdown "$file" | textutil -format html -convert doc -output "`basename "$file" | sed 's/\.txt//'`.doc" -stdin
+done
+
+exit
